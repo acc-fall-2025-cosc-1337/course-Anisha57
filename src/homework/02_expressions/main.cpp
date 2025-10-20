4f4ae6806a250a5aab223f7ed9cd41b93fa9f09d
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-10-20 18:56:06 -0500

    Implement meal total calculation with tax and tip
    Added functionality to calculate meal total including tax and tip.

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,32 @@
-﻿//write include statements
+// src/homework/02_expressions/main.cpp
+#include <iostream>
+#include <iomanip>
+#include "hwexpressions.h"
 
-
-//write namespace using statement for cout
+using namespace std;
 
 int main()
 {
-	
+    double meal_amount, tip_rate, sales_tax_amount, tip_amount, total;
 
-	return 0;
+    cout << "Enter meal amount: ";
+    cin >> meal_amount;
+
+    cout << "Enter tip rate (for 15% enter 0.15): ";
+    cin >> tip_rate;
+
+    sales_tax_amount = get_sales_tax_amount(meal_amount);
+    tip_amount = get_tip_amount(meal_amount, tip_rate);
+    total = meal_amount + sales_tax_amount + tip_amount;
+
+    cout << fixed << setprecision(2);
+    cout << "\nReceipt:\n";
+    cout << "------------------------\n";
+    cout << "Meal Amount:   $" << setw(8) << meal_amount << endl;
+    cout << "Sales Tax:     $" << setw(8) << sales_tax_amount << endl;
+    cout << "Tip Amount:    $" << setw(8) << tip_amount << endl;
+    cout << "------------------------\n";
+    cout << "Total:         $" << setw(8) << total << endl;
+
+    return 0;
 }
