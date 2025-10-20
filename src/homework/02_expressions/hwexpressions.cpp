970c0d90469d5e4a7f2e6f1949b64c4359c4332d
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-10-20 18:53:58 -0500

    Implement sales tax and tip calculation functions

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,13 +1,12 @@
+// src/homework/02_expressions/hwexpressions.cpp
 #include "hwexpressions.h"
 
-//example
-int add_numbers(int num1, int num2)
+double get_sales_tax_amount(double meal_amount)
 {
-	return num1 + num2;
+    return meal_amount * tax_rate;
 }
 
-//write function code here
-
-
-
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+    return meal_amount * tip_rate;
+}
