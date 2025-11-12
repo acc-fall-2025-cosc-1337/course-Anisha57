b0535c6740989ab0c53a95d766477a3f3d7eab57
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-11-11 19:22:00 -0600

    Refactor main menu and input handling

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,47 +1,46 @@
+// main.cpp
 #include <iostream>
 #include "decisions.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "MAIN MENU\n\n";
-    std::cout << "1-Letter grade using if\n";
-    std::cout << "2-Letter grade using switch\n";
-    std::cout << "3-Exit\n\n";
-
-    std::cout << "Enter option: ";
     int option;
-    if (!(std::cin >> option)) {
-        std::cout << "Invalid input. Exiting.\n";
-        return 1;
+    int grade;
+
+    cout << "MAIN MENU\n";
+    cout << "1 - Letter grade using if\n";
+    cout << "2 - Letter grade using switch\n";
+    cout << "3 - Exit\n";
+    cout << "Enter your choice: ";
+    cin >> option;
+
+    if (option == 1 || option == 2)
+    {
+        cout << "Enter a numerical grade (0 - 100): ";
+        cin >> grade;
+
+        if (grade >= 0 && grade <= 100)
+        {
+            if (option == 1)
+                cout << "Letter grade: " << get_letter_grade_using_if(grade) << endl;
+            else
+                cout << "Letter grade: " << get_letter_grade_using_switch(grade) << endl;
+        }
+        else
+        {
+            cout << "Error: Grade is out of range!" << endl;
+        }
+    }
+    else if (option == 3)
+    {
+        cout << "Goodbye!" << endl;
+    }
+    else
+    {
+        cout << "Invalid choice!" << endl;
     }
 
-    if (option == 3) {
-        std::cout << "Exiting program.\n";
-        return 0;
-    } else if (option == 1 || option == 2) {
-        std::cout << "Enter numerical grade (0 - 100): ";
-        int grade;
-        if (!(std::cin >> grade)) {
-            std::cout << "Invalid input. Exiting.\n";
-            return 1;
-        }
-
-        if (grade < 0 || grade > 100) {
-            std::cout << "Number out of range. Please enter 0..100.\n";
-            return 0;
-        }
-
-        std::string letter;
-        if (option == 1) {
-            letter = get_letter_grade_using_if(grade);
-        } else {
-            letter = get_letter_grade_using_switch(grade);
-        }
-
-        std::cout << "Letter grade: " << letter << "\n";
-        return 0;
-    } else {
-        std::cout << "Unknown option. Exiting.\n";
-        return 0;
-    }
+    return 0;
 }
+
