89326fcc8f3e618b10ab18f06a975d1216f2c18f
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-11-11 19:02:22 -0600

    Refactor data_types.h and add add_numbers prototype
    Removed unnecessary preprocessor directives and added function prototype for add_numbers.

--- src/homework/01_data_types/data_types.h
+++ src/homework/01_data_types/data_types.h
@@ -1,11 +1,4 @@
-// src/homework/01_data_types/data_types.h
-#ifndef DATA_TYPES_H
-#define DATA_TYPES_H
+﻿//example
+int add_numbers(int num1, int num2);
 
-// Function prototype
-int multiply_numbers(int num1, int num2);
-
-#endif
-
-
-
+//write function prototype here
