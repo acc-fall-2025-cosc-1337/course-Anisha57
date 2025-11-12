a3652b9095b6a66639a32e6550bbdc92416f73bd
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-11-11 19:20:35 -0600

    Refactor function signatures in decisions.h

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,13 +1,11 @@
+// decisions.h
 #ifndef DECISIONS_H
 #define DECISIONS_H
 
 #include <string>
+using std::string;
 
-// Return letter grade using if-else-if
-// Precondition: numerical_grade is in [0,100] (main will validate)
-std::string get_letter_grade_using_if(int numerical_grade);
+string get_letter_grade_using_if(int grade);
+string get_letter_grade_using_switch(int grade);
 
-// Return letter grade using switch (you may call with 0..100)
-std::string get_letter_grade_using_switch(int numerical_grade);
-
-#endif // DECISIONS_H
+#endif
