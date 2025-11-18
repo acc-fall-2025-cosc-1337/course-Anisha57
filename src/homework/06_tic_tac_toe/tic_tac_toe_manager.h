dc399b3d4656c8285a9e3e0a00c0fff94eec20a5
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-11-17 21:26:17 -0600

    Implement TicTacToeManager class
    Added TicTacToeManager class to manage multiple games.

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,23 @@
-//h
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+
+#include <vector>
+#include <string>
+#include "TicTacToe.h"  // include your TicTacToe class
+
+class TicTacToeManager
+{
+private:
+    std::vector<TicTacToe> games;  // stores all games
+    int x_win{0};
+    int o_win{0};
+    int ties{0};
+
+    void update_winner_count(const std::string& winner);
+
+public:
+    void save_game(TicTacToe b);  // adds a game and updates counts
+    void get_winner_total(int& x, int& o, int& t);  // get totals
+};
+
+#endif
