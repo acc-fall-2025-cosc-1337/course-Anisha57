852478d38835d2ab5bca3848008f28084fb93d88
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-11-20 23:24:08 -0600

    Update hwexpressions.cpp

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,9 +1,6 @@
 // hwexpressions.cpp
 #include "hwexpressions.h"
 
-// Constant tax rate of 6.75%
-const double tax_rate = 0.0675;
-
 double get_sales_tax_amount(double meal_amount)
 {
     return meal_amount * tax_rate;
