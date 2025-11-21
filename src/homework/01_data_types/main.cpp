d5f8f2da9e9344b77cb70fecc5f3921b7064d6ac
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-11-21 09:55:39 +0545

    Refactor main function to simplify output

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include "data_types.h"
 
-int main() {
-    // Display 7 x 7 = 49
-    int result1 = multiply_numbers(7, 7);
-    std::cout << "7 x 7 = " << result1 << std::endl;
-    
-    // Display 5 x 5 = 25  
-    int result2 = multiply_numbers(5, 5);
-    std::cout << "5 x 5 = " << result2 << std::endl;
-    
+int main()
+{
+    std::cout << "7 x 7 = " << multiply_numbers(7, 7) << "\n";
+    std::cout << "5 x 5 = " << multiply_numbers(5, 5) << "\n";
+
     return 0;
 }
