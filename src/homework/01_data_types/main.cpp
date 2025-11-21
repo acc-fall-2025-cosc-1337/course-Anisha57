fd83df1636a23e32767289b06b83ce6a1bdb1397
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-11-21 09:28:43 +0545

    Implement multiplication display in main function
    Added multiplication display for 7 x 7 and 5 x 5.

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,14 @@
-﻿//write include statements
+#include <iostream>
+#include "data_types.h"
 
-
-//write namespace using statement for cout
-
-
-int main()
-{
-	
-
-	return 0;
+int main() {
+    // Display 7 x 7 = 49
+    int result1 = multiply_numbers(7, 7);
+    std::cout << "7 x 7 = " << result1 << std::endl;
+    
+    // Display 5 x 5 = 25  
+    int result2 = multiply_numbers(5, 5);
+    std::cout << "5 x 5 = " << result2 << std::endl;
+    
+    return 0;
 }
