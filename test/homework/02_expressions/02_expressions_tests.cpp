e98ad41c8fd70554d6880747a72edcfeb34fab43
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-11-20 23:25:11 -0600

    Refactor tests to use assert instead of Catch2
    Replaced Catch2 testing framework with assert statements for sales tax and tip amount calculations.

--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -1,16 +1,16 @@
 // 02_expressions_tests.cpp
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
 #include "hwexpressions.h"
+#include <cassert>
 
-TEST_CASE("Verify get_sales_tax_amount function")
+int main()
 {
-    REQUIRE(get_sales_tax_amount(10) == Approx(0.675));
-    REQUIRE(get_sales_tax_amount(20) == Approx(1.35));
-}
+    // Test sales tax
+    assert(get_sales_tax_amount(10.0) == 0.675);
+    assert(get_sales_tax_amount(20.0) == 1.35);
 
-TEST_CASE("Verify get_tip_amount function")
-{
-    REQUIRE(get_tip_amount(20, 0.15) == Approx(3.0));
-    REQUIRE(get_tip_amount(30, 0.20) == Approx(6.0));
+    // Test tip amount
+    assert(get_tip_amount(20.0, 0.10) == 2.0);
+    assert(get_tip_amount(50.0, 0.20) == 10.0);
+
+    return 0;
 }
