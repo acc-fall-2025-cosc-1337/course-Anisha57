8213402299757ed93e953e06f245a759b98cbb5a
Anisha57 <anisha.bhatta@g.austincc.edu>
2025-12-03 18:11:49 -0600

    Hw06 complete

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,28 +1,38 @@
 #include "tic_tac_toe.h"
 #include <iostream>
 
-int main()
-{
+int main() {
     TicTacToe game;
-
-    std::string player;
+    std::string first_player;
+    char continue_game = 'y';
+    
     std::cout << "Enter first player (X or O): ";
-    std::cin >> player;
-
-    game.start_game(player);
-
-    int position;
-    while (!game.game_over())
-    {
-        std::cout << "Enter position (1-9): ";
-        std::cin >> position;
-
-        game.mark_board(position);
+    std::cin >> first_player;
+    
+    game.start_game(first_player);
+    
+    while (continue_game == 'y' || continue_game == 'Y') {
+        
+        while (!game.game_over()) {
+            game.display_board();
+            std::cout << "Player " << game.get_player() << " enter position (1-9): ";
+            int position;
+            std::cin >> position;
+            game.mark_board(position);
+        }
+        
         game.display_board();
-        std::cout << "\n";
+        std::cout << "Game over!\n";
+        
+        std::cout << "Play again? (y/n): ";
+        std::cin >> continue_game;
+        
+        if (continue_game == 'y' || continue_game == 'Y') {
+            std::cout << "Enter first player (X or O): ";
+            std::cin >> first_player;
+            game.start_game(first_player); 
+        }
     }
-
-    std::cout << "Game over! Board is full.\n";
-
+    
     return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,73 +1,56 @@
 #include "tic_tac_toe.h"
 #include <iostream>
 
-bool TicTacToe::game_over()
-{
+
+bool TicTacToe::game_over() {
     return check_board_full();
 }
 
-void TicTacToe::start_game(std::string first_player)
-{
+void TicTacToe::start_game(std::string first_player) {
     player = first_player;
     clear_board();
 }
 
-void TicTacToe::mark_board(int position)
-{
-    pegs[position - 1] = player;
+void TicTacToe::mark_board(int position) {
+    int index = position - 1;
+    pegs[index] = player;
     set_next_player();
 }
 
-std::string TicTacToe::get_player() const
-{
+std::string TicTacToe::get_player() const {
     return player;
 }
 
-void TicTacToe::display_board() const
-{
-    for (int i = 0; i < 9; i++)
-    {
-        std::cout << pegs[i];
-
-        if ((i + 1) % 3 == 0)
-        {
-            std::cout << "\n";
-        }
-        else
-        {
-            std::cout << "|";
-        }
-    }
+void TicTacToe::display_board() const {
+    std::cout << "\n";
+    std::cout << pegs[0] << "|" << pegs[1] << "|" << pegs[2] << "\n";
+    std::cout << "-+-+-\n";
+    std::cout << pegs[3] << "|" << pegs[4] << "|" << pegs[5] << "\n";
+    std::cout << "-+-+-\n";
+    std::cout << pegs[6] << "|" << pegs[7] << "|" << pegs[8] << "\n";
+    std::cout << "\n";
 }
 
-void TicTacToe::set_next_player()
-{
-    if (player == "X")
-    {
+
+void TicTacToe::set_next_player() {
+    if (player == "X") {
         player = "O";
-    }
-    else
-    {
+    } else {
         player = "X";
     }
 }
 
-bool TicTacToe::check_board_full()
-{
-    for (auto& peg : pegs)
-    {
-        if (peg == " ")
-        {
-            return false;
+bool TicTacToe::check_board_full() {
+    for (int i = 0; i < 9; i++) {
+        if (pegs[i] == " ") {
+            return false;  
         }
     }
-    return true;
+    return true;  
 }
 
-void TicTacToe::clear_board()
-{
-    for (auto& peg : pegs)
-    {
-        peg = " ";
+void TicTacToe::clear_board() {
+    for (int i = 0; i < 9; i++) {
+        pegs[i] = " ";
     }
 }
